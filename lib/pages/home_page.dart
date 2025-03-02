43047a267844c32acda797ea654c141e9adf29e6
Raed Abdallah <raed.dev@gmail.com>
2025-03-02 09:58:32 +0000

    adding a cool rect

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,7 +18,19 @@ class HomePage extends StatelessWidget {
         child: Stack(
           fit: StackFit.expand,
           alignment: const Alignment(0.0, 0.0),
-          children: [],
+          children: [
+            Positioned(
+              top: 75.0,
+              left: 91.5,
+              width: 210.0,
+              height: 199.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color(0xffc4c4c4),
+                    borderRadius: BorderRadius.circular(0.0)),
+              ),
+            )
+          ],
         ),
       ),
     );
